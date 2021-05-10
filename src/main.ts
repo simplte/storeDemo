31b7fc72c8aceeb261843786e63406372c61a1a9
bqc <6575734@qq.com>
2021-05-10 22:52:48 +0800

    vuex-element-axios

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
-createApp(App).use(router).mount('#app')
+import store from './store/index';
+
+
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css'
+createApp(App).use(router).use(store).use(ElementPlus).mount('#app')
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,18 +1,46 @@
 <template>
-<div>
-    home
-</div>
+  <div>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import axios from '../utils/axios'
 export default defineComponent({
   name: "home",
- 
+  data() {
+      return {
+          tableData: [{
+            date: '2016-05-02',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1518 弄'
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1517 弄'
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+          }, {
+            date: '2016-05-03',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1516 弄'
+          }]
+        }
+  },
+  setup() {
+      axios.get('/users/XPoet')
+      .then((res) => { console.log('res: ', res) })
+      .catch((err) => { console.log('err: ', err) }) }
+  
 });
 </script>
 
 <style>
-
 </style>
