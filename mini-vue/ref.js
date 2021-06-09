8a3b587a44bcfca853a8c9cc504ebd2b8ee08c5b
bqc <6575734@qq.com>
2021-06-09 22:23:32 +0800

    reactive实现

--- mini-vue/ref.js
+++ mini-vue/ref.js
@@ -1,39 +1,83 @@
-let effectCb;
+let currentEffect;
 class Dep {
     constructor(val) {
-        this._value = val;
+        // this._value = val;
         this.effectArr = new Set()
     }
-    get value() {
-        this.depend()
-        return this._value;
-    }
-    set value(val) {
-        this._value = val;
-        this.notice();
-    }
+    // get value() {
+    //     this.depend()
+    //     return this._value;
+    // }
+    // set value(val) {
+    //     this._value = val;
+    //     this.notice();
+    // }
     notice() {
         this.effectArr.forEach(effect => {
             effect();
         });
     }
     depend () {
-        if(effectCb) {
-            this.effectArr.add(effectCb)
+        if(currentEffect) {
+            this.effectArr.add(currentEffect)
         }
 
     }
 }
 function effectWatch(effect) {
-    effectCb = effect;
+    currentEffect = effect;
     effect()
-    effectCb = null;
+    currentEffect = null;
 }
 // b的值依赖于value, 当value发生改变时需要触发注册，执行对应动作
-let dep = new Dep(10)
-let b = 0;
-effectWatch(() => {
-    b = dep.value + 10;
-    console.log(b);
+// let dep = new Dep(10)
+// let b = 0;
+// effectWatch(() => {
+//     b = dep.value + 10;
+//     console.log(b);
+// })
+// dep.value = 1;
+const targetMap = new Map();
+function reactive(raw) {
+    return new Proxy(raw, {
+        get(target, key) {
+            let depMap = targetMap.get(target);
+            if(!depMap) {
+                depMap = new Map();
+                targetMap.set(target, depMap)
+            }
+            let dep = depMap.get(key);
+            if(!dep){
+                dep = new Dep();
+                depMap.set(key, dep)
+            }
+            dep.depend();
+            return Reflect.get(target, key)
+        },
+        set(target, key, val) {
+            let depMap = targetMap.get(target);
+            if(!depMap) {
+                depMap = new Dep();
+                targetMap.set(target, depMap);
+            }
+            let dep = depMap.get(key);
+            if(!dep) {
+                dep = new Map();
+                depMap.set(key, dep)
+            }
+            let res = Reflect.set(target, key, val);
+            dep.notice();
+            return res
+        }
+    })
+}
+const user = reactive({
+    age: 1
 })
-dep.value = 1;
+let double ;
+effectWatch(() => {
+    console.log("--------reactive------")
+    double = user.age;
+    console.log(double)
+})
+user.age = 2;
